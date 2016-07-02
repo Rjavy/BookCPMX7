66e299a8458e7ef1fafe4169d90d9335f71fa092
Rodrigo Rodriguez <rodrigor@vsisoft.com>
2016-07-02 13:35:31 -0500

    listo

--- cpmx.js
+++ cpmx.js
@@ -17,5 +17,19 @@ setTimeout(function(){
 setTimeout(function(){
   $('div').addClass('animated hinge');
 }, 2500);
-
+setTimeout(function(){
+  $('li').addClass('animated lightSpeedOut');
+}, 3000);
+setTimeout(function(){
+  $('img').addClass('animated hinge');
+}, 3500);
+setTimeout(function(){
+  $('select').addClass('animated lightSpeedOut');
+}, 4000);
+setTimeout(function(){
+  $('a').addClass('animated lightSpeedOut');
+}, 4500)
+setTimeout(function(){
+  $('body').addClass('animated hinge');
+}, 5000)
 })();

046a2f1945b314a649fe7408b367f0c09b8e93dc
Rodrigo Rodriguez <rodrigor@vsisoft.com>
2016-07-02 13:31:30 -0500

    me largo!

--- cpmx.js
+++ cpmx.js
@@ -1,5 +1,21 @@
 (function() {
-  
-  debugger
-  $('h1').animateCss('tada');
+
+setTimeout(function(){
+  $('h1').addClass('animated rotateOut');
+}, 500);
+
+setTimeout(function(){
+  $('h2').addClass('animated rotateOut');
+}, 1000);
+
+setTimeout(function(){
+  $('h3').addClass('animated rotateOut');
+}, 1500);
+setTimeout(function(){
+  $('p').addClass('animated rotateOut');
+}, 2000);
+setTimeout(function(){
+  $('div').addClass('animated hinge');
+}, 2500);
+
 })();
